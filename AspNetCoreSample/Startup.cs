9a794ccdf85340a891ef608be84f860458e9da17
Rafal Kaszczuk <havi1989@gmail.com>
2018-10-03 12:01:39 +0200

    Init

---
+++ AspNetCoreSample/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace AspNetCoreSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "SampleAPI", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "Nagłowek używany do autoryzacji",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>()
+                {
+                  { "Bearer", new string[]{ } }
+                });
+            });
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(cfg => {
+                cfg.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = "http://localhost:9011",
+                    ValidAudience = "http://localhost:9011",
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superTajnyKlucz123$"))
+                };
+                cfg.SaveToken = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleAPI");
+            });
+
+            app.UseAuthentication();
+            app.UseMvc();
+
+        }
+    }
+}
---
+++ IoCAutofacSamples/IoCAutofacConsoleSample/Program.cs
@@ -0,0 +1,133 @@
+﻿using Autofac;
+using IoCAutofacConsoleSample.SampleImp;
+using IoCAutofacInterfacesSample.SampleInterfaces;
+using IoCAutofacLibSample;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace IoCAutofacConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicRegisterTest();
+            RegisterWithCtor();
+            RegisterWithParameter();
+            RegisterWithoutScope();
+            RegisterWithScope();
+            RegisterSignleInstance();
+            RegisterWithCtorInjection();
+            RegisterWithAssemblyScaning();
+            Console.ReadKey();
+        }
+        static void PrintMethodName()
+        {
+            StackFrame frame = new StackFrame(1);
+            Console.WriteLine($"--- {frame.GetMethod().Name} ---");
+        }
+
+        static void BasicRegisterTest()
+        {
+            PrintMethodName();
+            ///Tworzymy builder
+            var builder = new ContainerBuilder();
+            ///Rejestrujemy typ Foo jako IFoo
+            builder.RegisterType<Foo>().As<IFoo>();
+            ///Budujemy builder do kontenera
+            var container = builder.Build();
+            ///Testujemy rozwiązanie typu IFoo na Foo
+            Console.WriteLine(container.Resolve<IFoo>().GetHello());
+        }
+        static void RegisterWithCtor()
+        {
+            PrintMethodName();
+            ///Tworzymy builder
+            var builder = new ContainerBuilder();
+            ///Rejestrujemy typ Foo2 jako IFoo. W trakcie rejestracji wykonujemy kontruktor
+            builder.Register(x=> new Foo2("Rafał")).As<IFoo>();
+            ///Budujemy builder do kontenera
+            var container = builder.Build();
+            ///Testujemy rozwiązanie typu IFoo na Foo
+            Console.WriteLine(container.Resolve<IFoo>().GetHello());
+        }
+        static void RegisterWithParameter()
+        {
+            PrintMethodName();
+            var builder = new ContainerBuilder();
+            builder.Register<IFoo>((x, p) =>
+            {
+                var paramValue = p.TypedAs<string>();
+                if (String.IsNullOrEmpty(paramValue))
+                {
+                    return new Foo();
+                }
+                else
+                {
+                    return new Foo2(paramValue);
+                }
+            });
+            var container = builder.Build();
+            Console.WriteLine(container.Resolve<IFoo>(new TypedParameter(typeof(string),null)).GetHello());
+            Console.WriteLine(container.Resolve<IFoo>(new TypedParameter(typeof(string),"Rafał")).GetHello());
+        }
+        static void RegisterWithoutScope()
+        {
+            PrintMethodName();
+            var builder = new ContainerBuilder();
+            builder.Register(x => new Counter()).AsSelf().AsImplementedInterfaces();
+            var container = builder.Build();
+            Console.WriteLine(container.Resolve<Counter>().GetNext());
+            Console.WriteLine(container.Resolve<Counter>().GetNext());
+            Console.WriteLine(container.Resolve<ICounter>().GetNext());
+        }
+        static void RegisterWithScope()
+        {
+            PrintMethodName();
+            var builder = new ContainerBuilder();
+            builder.Register(x => new Counter()).AsSelf().InstancePerLifetimeScope();
+            var container = builder.Build();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                Console.WriteLine(scope.Resolve<Counter>().GetNext());
+                Console.WriteLine(scope.Resolve<Counter>().GetNext());
+            }
+            Console.WriteLine(container.Resolve<Counter>().GetNext());
+        }
+        static void RegisterSignleInstance()
+        {
+            PrintMethodName();
+            var builder = new ContainerBuilder();
+            builder.Register(x => new Counter()).AsSelf().SingleInstance();
+            var container = builder.Build();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                Console.WriteLine(scope.Resolve<Counter>().GetNext());
+                Console.WriteLine(scope.Resolve<Counter>().GetNext());
+            }
+            Console.WriteLine(container.Resolve<Counter>().GetNext());
+        }
+        static void RegisterWithCtorInjection()
+        {
+            PrintMethodName();
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Foo>().As<IFoo>();
+            builder.RegisterType<ConsolePrinter>().AsSelf();
+            var container = builder.Build();
+            container.Resolve<ConsolePrinter>().Print();
+        }
+        static void RegisterWithAssemblyScaning()
+        {
+            PrintMethodName();
+            var builder = new ContainerBuilder();
+            builder.Register(x=> new Foo2("Rafał")).As<IFoo>();
+          
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            builder.RegisterAssemblyTypes(assemblies).Where(x => x.GetInterfaces().Contains(typeof(IPrinter))).AsImplementedInterfaces();
+
+            var container = builder.Build();
+            container.Resolve<IPrinter>().Print();
+        }
+    }
+}
